b5c1019acdfce5455581db4da41698479595fbb1
lebel-hounkanrin <lebelhounkanrin@gmail.com>
2022-12-10 22:55:31 +0100

    update react component

--- Rclient/src/CommentList.js
+++ Rclient/src/CommentList.js
@@ -1,7 +1,11 @@
 
 export default ({comments}) => {
     const renderedComment = Object.values(comments).map(comment => {
-        return <p>{comment.content}</p>
+        let content;
+        if (comment.status === "approved") content = comment.content;
+        if(comment.status ==="rejected") content = "This comment has been rejected";
+        if(comment.status === "pending") content = "This comment is awaiting moderation"
+        return <p>{content}</p>
     })
     return (
         <div>{renderedComment}</div>
